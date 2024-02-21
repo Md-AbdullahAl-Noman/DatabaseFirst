e8e26727c3c851ff619cae88ce53cc43a9933bb4
Acer <Acer@AL-NOMAN-LAPTOP>
2024-02-21 14:39:14 +0600

    Add project files.

---
+++ DatabaseFirst/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseFirstEntities db =new DatabaseFirstEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ DatabaseFirst/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseFirst.Controllers
+{
+    public class StudentController : Controller
+    {
+        DatabaseFirstEntities db = new DatabaseFirstEntities();
+        // GET: Student
+        public ActionResult ViewDetails()
+        { var data = db.Students;
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Create(Student obj)
+        {
+            db.Students.Add(obj);
+            int a=db.SaveChanges();
+            if (a > 0)
+            {
+                TempData["MESSAGE"] = "Inserted successfully";
+                return RedirectToAction("ViewDetails");
+            }
+            else {
+
+                TempData["MESSAGE"] = "Cant inssert";
+            }
+            return View();
+        }
+
+
+    }
+}
